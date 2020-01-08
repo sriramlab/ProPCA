ddf2d61049c7e7c0745fc5c53639fdc3b2770019
Alec <alecmchiu@gmail.com>
2020-01-08 13:38:45 -0800

    Corrected default parameters and added misc files from manuscript

--- include/arguments.h
+++ include/arguments.h
@@ -195,13 +195,13 @@ void parse_args(int argc, char const *argv[]){
 	
 	// Setting Default Values
 	command_line_opts.num_of_evec=5;
-	command_line_opts.max_iterations=2*command_line_opts.num_of_evec;
+	command_line_opts.max_iterations=2+command_line_opts.num_of_evec;
 	command_line_opts.getaccuracy=false;
 	command_line_opts.debugmode=false;
 	command_line_opts.OUTPUT_PATH = "fastppca_";
 	bool got_genotype_file=false;
 	command_line_opts.var_normalize=false;
-	command_line_opts.l=5;
+	command_line_opts.l=command_line_opts.num_of_evec;
 	command_line_opts.accelerated_em=0;
 	command_line_opts.convergence_limit= -1.0;
 	command_line_opts.memory_efficient=false;
@@ -224,10 +224,10 @@ void parse_args(int argc, char const *argv[]){
 		ConfigFile cfg(cfg_filename);
 		got_genotype_file=cfg.keyExists("genotype");
 		command_line_opts.num_of_evec=cfg.getValueOfKey<int>("num_evec",5);
-		command_line_opts.max_iterations = cfg.getValueOfKey<int>("max_iterations",2*command_line_opts.num_of_evec);
+		command_line_opts.max_iterations = cfg.getValueOfKey<int>("max_iterations",2+command_line_opts.num_of_evec);
 		command_line_opts.getaccuracy=cfg.getValueOfKey<bool>("accuracy",false);
 		command_line_opts.debugmode=cfg.getValueOfKey<bool>("debug",false);
-		command_line_opts.l=cfg.getValueOfKey<int>("l",5);
+		command_line_opts.l=cfg.getValueOfKey<int>("l",command_line_opts.num_of_evec);
 		command_line_opts.OUTPUT_PATH = cfg.getValueOfKey<string>("output_path",string("fastppca_"));
 		command_line_opts.GENOTYPE_FILE_PATH = cfg.getValueOfKey<string>("genotype",string(""));
 		command_line_opts.convergence_limit =cfg.getValueOfKey<double>("convergence_limit",-1.0);
@@ -321,7 +321,7 @@ void parse_args(int argc, char const *argv[]){
 					command_line_opts.text_version=true;
 		}
 		if(!got_max_iter)
-			command_line_opts.max_iterations = 2*command_line_opts.num_of_evec;
+			command_line_opts.max_iterations = 2+command_line_opts.num_of_evec;
 
 	}
 	
